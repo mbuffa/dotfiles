a7a721820f585186fc663c3644ddeabd761b9143
Maxime Buffa <mbuffa@gmail.com>
2017-02-04 13:03:59 +0100

    Move home content to root.
    - Move gnome-setup.sh to .bin
    - Delete symlink.sh

--- symlink.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd home
-
-$PWD = `pwd`
-
-ln -s $PWD/.zshrc ~/
-ln -s $PWD/.gitconfig ~/
-ln -s $PWD/.bin ~/
-ln -s $PWD/.config/sublime-text-3/Packages/User ~/.config/sublime-text-3/Packages/
