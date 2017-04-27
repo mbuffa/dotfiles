18a452677e5a0c88652a4e8b24955b7494afd1ef
Maxime Buffa <mbuffa@gmail.com>
2017-04-27 18:47:38 +0200

    Remove Linux related files.

--- gnome-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-## Make screen magnifier usable.
-gsettings set org.gnome.desktop.a11y.magnifier focus-tracking none
-gsettings set org.gnome.desktop.a11y.magnifier caret-tracking none
